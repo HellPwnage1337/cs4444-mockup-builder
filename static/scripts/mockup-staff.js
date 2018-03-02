8f48758498beb7e688b288e20d8ae668c041dba6
Shawn Lutch <shawn@chaoticweg.cc>
2018-03-01 23:39:54 -0600

    add modal (broken)
    jquery.data() is fucking up

--- static/scripts/mockup-staff.js
+++ static/scripts/mockup-staff.js
@@ -4,9 +4,24 @@ const statuses = {
     '7': { "status": Status['AwaitingOrder'], "help": Request['Help'] }
 };
 
-$(() => {
+$(window).ready(() => {
     for (let i in statuses) {
         $('#status-' + i).text(statuses[i]["status"]);
         $('#help-' + i).text(statuses[i]["help"]);
     }
+
+    $('.show-table-modal').click(() => {
+        let tableNumber = $(this).data('tableNumber');
+        console.log('clicked on table %s', tableNumber);
+
+        let tableStatus = statuses.hasOwnProperty(tableNumber) ? statuses[tableNumber] : {
+            "status": Status['Inactive'], "help": Request['None']
+        };
+
+        $('#table-modal-number').text(tableNumber);
+        $('#table-modal-status').text(tableStatus['status']);
+        $('#table-modal-help').text(tableStatus['help']);
+
+        $('#modal-show').click();
+    });
 });
