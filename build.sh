dd3ac0c1aaae5ab80ce98fdfd44caf8bb512a025
ChaoticWeg <shawn.lutch94@gmail.com>
2018-03-02 00:45:01 -0600

    add bash build script

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-echo "Not supported yet!"
-exit 1
+clear
+
+echo pre-cleaning output directories
+node clean.js output/ temp_output/
+
+echo
+echo copying new files from static directory
+cp -r ./static ./output
+
+echo
+node build.js
+
+echo
+echo copying new files from temp_output/ to output/
+cp -r ./temp_output/* ./output
+
+echo
+echo cleaning temp_output directory once more
+node clean.js temp_output/
+
+echo
+node server.js
